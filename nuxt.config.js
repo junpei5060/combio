41fa735ddcd69910c72a14ba9fd1162ef77dfaa7
junpei5060 <junpei.nakazawa.5060@gmail.com>
2020-11-29 22:15:07 +0900

    【add】githubpagesdeploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'combio',
+    title: '師弟.com マンツーマンプロ上司サービス',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'combio/favicon.ico' }
     ]
   },
 
@@ -41,5 +41,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+  },
+  router: {
+    base: '/combio/',
+    mode:history
+  },
 }
