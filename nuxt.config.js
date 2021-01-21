fdafda30c73965b87f0c35885c1ba08c71826c39
junpei5060 <junpei.nakazawa.5060@gmail.com>
2021-01-21 21:42:48 +0900

    完成

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "meeeLook/favicon.ico" },
+    link: [{ rel: "icon", type: "image/x-icon", href: '/favicon.ico' },
     { rel: "stylesheet", href:"https://use.fontawesome.com/releases/v5.6.1/css/all.css"}]
           
   },
